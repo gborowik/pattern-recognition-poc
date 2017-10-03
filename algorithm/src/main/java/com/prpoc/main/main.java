2e49d9c6517df411b1acdcdcc9d6fd1839204f4e
Grzegorz <borowik.grzegorz@gmail.com>
2017-10-03 10:44:26 +0200

    initial structures

--- algorithm/src/main/java/com/prpoc/main/main.java
+++ algorithm/src/main/java/com/prpoc/main/main.java
@@ -1,16 +1,32 @@
 package com.prpoc.main;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Grzegorz Borowik on 2017-10-03 10:11 AM.
  * e-mail: borowik.grzegorz@gmail.com
  */
 
-public class main{
+public class main {
+
+    List<Integer> properImage = new ArrayList<>(Arrays.asList
+            (0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0));
+
+    List<Integer> wrongImage = new ArrayList<>(Arrays.asList
+            (1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1));
+
+    List<Neuron> inputNeurons = new ArrayList<>();
+    List<Neuron> outputNeurons = new ArrayList<>();
+
+    List<Synapse> synapses = new ArrayList<>();
+
+    Integer tick;
 
 
 
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
 
