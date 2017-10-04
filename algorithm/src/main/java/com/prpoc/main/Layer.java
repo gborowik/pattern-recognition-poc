f7f19abf2f055c8750c6e6a01ba4dd4b6de6c04f
Grzegorz <borowik.grzegorz@gmail.com>
2017-10-04 16:38:30 +0200

    methods added

--- algorithm/src/main/java/com/prpoc/main/Layer.java
+++ algorithm/src/main/java/com/prpoc/main/Layer.java
@@ -1,10 +1,22 @@
 package com.prpoc.main;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
 public abstract class Layer {
 
-    protected Set<Node> nodes;
+    protected Set<Node> nodes = new HashSet<>();
+
+
+    public void addNode(Node<?> node) {
+        nodes.add(node);
+    }
+
+    public void addNodes(Collection<Node<?>> ns) {
+        nodes.addAll(ns);
+    }
+
 
     public abstract String toString();
 }
--- algorithm/src/main/java/com/prpoc/main/Network.java
+++ algorithm/src/main/java/com/prpoc/main/Network.java
@@ -1,14 +1,62 @@
 package com.prpoc.main;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
 public abstract class Network {
 
-    protected Set<Node> nodes;
+    protected Set<Node> nodes = new HashSet<>();
 
-    protected Set<Layer> layers;
+    protected Set<Node> inNodes = new HashSet<>();
 
-    protected Set<DirectedEdge> directedEdges;
+    protected Set<Node> outNodes = new HashSet<>();
+
+    protected Set<DirectedEdge> directedEdges = new HashSet<>();
+
+    protected Set<Layer> layers = new HashSet<>();
+
+
+    public void addNode(Node<?> node) {
+        nodes.add(node);
+    }
+
+    public void addNodes(Collection<Node<?>> ns) {
+        nodes.addAll(ns);
+    }
+
+    public void addInputNode(Node<?> node) {
+        inNodes.add(node);
+    }
+
+    public void addInputNodes(Collection<Node<?>> ns) {
+        inNodes.addAll(ns);
+    }
+
+    public void addOutputNode(Node<?> node) {
+        outNodes.add(node);
+    }
+
+    public void addOutputNodes(Collection<Node<?>> ns) {
+        outNodes.addAll(ns);
+    }
+
+    public void addEdge(DirectedEdge<?> edge) {
+        directedEdges.add(edge);
+    }
+
+    public void addEdges(Collection<DirectedEdge<?>> es) {
+        directedEdges.addAll(es);
+    }
+
+    public void addLayer(Layer layer) {
+        layers.add(layer);
+    }
+
+    public void addLayers(Collection<Layer> ls) {
+        layers.addAll(ls);
+    }
+
 
     public abstract String toString();
 
