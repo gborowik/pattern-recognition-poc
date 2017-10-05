2892a1bf626db1b2c6e4e9a129fc97988a6c8e99
Grzegorz Borowik <borowik.grzegorz@gmail.com>
2017-10-05 11:07:40 +0200

    SynapseFactory created

---
+++ algorithm/src/main/java/com/prpoc/main/SynapseFactory.java
@@ -0,0 +1,12 @@
+package com.prpoc.main;
+
+import java.util.Collection;
+
+public class SynapseFactory {
+
+    public Collection<DirectedEdge<?>> generateAllSynapsesBetweenLayers(Layer layer1, Layer layer2) {
+        return null;
+    }
+
+
+}
