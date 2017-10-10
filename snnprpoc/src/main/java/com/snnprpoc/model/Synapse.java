f39e22c84178364b3315827d418b31cb07391228
Grzegorz <borowik.grzegorz@gmail.com>
2017-10-10 14:21:11 +0200

    double -> Double, constructor update

--- snnprpoc/src/main/java/com/snnprpoc/model/Synapse.java
+++ snnprpoc/src/main/java/com/snnprpoc/model/Synapse.java
@@ -6,27 +6,28 @@ import static com.snnprpoc.main.Constants.defaultSynapseWeight;
 
 public class Synapse extends DirectedEdge<Neuron> {
 
-    private double weight = defaultSynapseWeight;
+    private Double weight;
 
     public Synapse(Neuron origin, Neuron end) {
         super(origin, end);
+        weight = defaultSynapseWeight;
     }
 
-    public Synapse(Neuron origin, Neuron end, double weight) {
+    public Synapse(Neuron origin, Neuron end, Double weight) {
         super(origin, end);
         this.weight = weight;
     }
 
-    public Synapse setWeight(double weight) {
+    public Synapse setWeight(Double weight) {
         this.weight = weight;
         return this;
     }
 
-    public double getWeight() {
+    public Double getWeight() {
         return weight;
     }
 
-    public Synapse fire(double value) {
+    public Synapse fire(Double value) {
         end.charge(value * weight);
         return this;
     }
